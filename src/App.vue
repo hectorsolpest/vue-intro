571d23f98cc1197ee1e0cec2a6b4eb8119c88e80
hectore <hectorsolpest@gmail.com>
2025-09-03 18:08:03 -0500

    add home cards

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue'
 import Hero from '@/components/Hero.vue'
+import HomeCards from '@/components/HomeCards.vue'
 </script>
 
 <template>
   <navbar/>
   <hero title="Become a Vue Dev motherfucker" subtitle="Find a fucking job !!!"/>
+  <home-cards/>
 </template>
 
 <style scoped>
