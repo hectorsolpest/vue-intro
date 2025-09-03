48ce541815accd0eaaa107a82bff9e2406439c0a
hectore <hectorsolpest@gmail.com>
2025-09-03 16:53:07 -0500

    add Hero component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue'
+import Hero from '@/components/Hero.vue'
 </script>
 
 <template>
   <navbar/>
+  <hero/>
 </template>
 
 <style scoped>
