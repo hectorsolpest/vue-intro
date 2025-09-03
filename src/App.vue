f509f316ab5b72b8d33283402ebcdf6e962a2d3f
hectore <hectorsolpest@gmail.com>
2025-09-03 09:40:18 -0500

    add tailwind to project

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,9 @@
 <script setup>
-import { ref,onMounted } from 'vue';
 
-let name=ref('Vue Master')
-let status=ref(true)
-const tasks=ref(['task 1', 'task 2', 'task 3'])
-const newTask = ref('')
-
-function changeStatus()
-{
-  status.value = status.value !== true;
-}
-function addTask() {
-  if(newTask.value.trim() !== ''){
-    tasks.value.push(newTask.value)
-  }
-  newTask.value = ''
-}
-function deleteTask(index) {
-  tasks.value.splice(index,1)
-}
-onMounted(
-  async ()=>{
-    try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/todos')
-      const data = await response.json()
-      tasks.value = data.map((task)=>task.title)
-    }catch {
-      console.log('error fetching data')
-    }
-  }
-)
 </script>
 
 <template>
-  <h1>Vue Jobs for {{name}}</h1>
-  <p v-if="status">User active</p>
-  <p v-else>User inactive</p>
-  <h3>
-    <ul>
-      <li v-for="(task,index) in tasks" :key="task">
-        {{task}}
-        <button @click="deleteTask(index)">X</button>
-      </li>
-    </ul>
-  </h3>
-  <button v-on:click="changeStatus">Change status</button>
-  <form @submit.prevent="addTask">
-    <label for="newTask">Add Task</label>
-    <input type="text" id="newTask" name="newTask" v-model="newTask">
-    <button type="submit">Add</button>
-  </form>
+  <div></div>
 </template>
 
 <style scoped>
