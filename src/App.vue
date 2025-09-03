e243cd9ab6488bb0a52a014408b087d9d14ee89d
hectore <hectorsolpest@gmail.com>
2025-09-03 09:10:42 -0500

    uso de formularios dinamicos

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,42 @@ import { ref } from 'vue';
 let name=ref('Vue Master')
 let status=ref(true)
 const tasks=ref(['task 1', 'task 2', 'task 3'])
-
+const newTask = ref('')
 
 function changeStatus()
 {
   status.value = status.value !== true;
 }
-
+function addTask() {
+  if(newTask.value.trim() !== ''){
+    tasks.value.push(newTask.value)
+  }
+  newTask.value = ''
+}
+function deleteTask(index) {
+  tasks.value.splice(index,1)
+}
 
 </script>
 
 <template>
   <h1>Vue Jobs for {{name}}</h1>
-<p v-if="status">User active</p>
-<p v-else>User inactive</p>
+  <p v-if="status">User active</p>
+  <p v-else>User inactive</p>
   <h3>
     <ul>
-      <li v-for="task in tasks" :key="task">{{task}}</li>
+      <li v-for="(task,index) in tasks" :key="task">
+        {{task}}
+        <button @click="deleteTask(index)">X</button>
+      </li>
     </ul>
   </h3>
   <button v-on:click="changeStatus">Change status</button>
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask">
+    <button type="submit">Add</button>
+  </form>
 </template>
 
 <style scoped>
