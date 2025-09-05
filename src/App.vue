22173854cc9ea7ef6ab98e1b0412c4b121787bdc
hectore <hectorsolpest@gmail.com>
2025-09-04 21:56:36 -0500

    add jobs list

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import Navbar from '@/components/Navbar.vue'
 import Hero from '@/components/Hero.vue'
 import HomeCards from '@/components/HomeCards.vue'
+import JobList from '@/components/JobList.vue'
 </script>
 
 <template>
   <navbar/>
   <hero title="Become a Vue Dev motherfucker" subtitle="Find a fucking job !!!"/>
   <home-cards/>
+  <job-list :limit="3" :show-button="false"/>
 </template>
 
 <style scoped>
