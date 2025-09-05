385b7c3ca8da5549db72266eed68f8614c9a2dad
hectore <hectorsolpest@gmail.com>
2025-09-04 22:11:26 -0500

    add router view

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue'
-import Hero from '@/components/Hero.vue'
-import HomeCards from '@/components/HomeCards.vue'
-import JobList from '@/components/JobList.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <navbar/>
-  <hero title="Become a Vue Dev motherfucker" subtitle="Find a fucking job !!!"/>
-  <home-cards/>
-  <job-list :limit="3" :show-button="false"/>
+  <RouterView/>
 </template>
 
 <style scoped>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Hero from '@/components/Hero.vue'
+import HomeCards from '@/components/HomeCards.vue'
+import JobList from '@/components/JobList.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <hero title="Become a Vue Dev motherfucker" subtitle="Find a fucking job !!!"/>
+  <home-cards/>
+  <job-list :limit="3" :show-button="false"/>
 </template>
