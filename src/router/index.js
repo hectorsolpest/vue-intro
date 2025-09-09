b598965a1c341d4a81b279fcb23e9d1248ccc717
hectore <hectorsolpest@gmail.com>
2025-09-08 21:08:42 -0500

    create edit job view

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import JobsView from '@/views/JobsView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
 import JobView from '@/views/JobView.vue'
 import AddJobView from '@/views/AddJobView.vue'
+import EditJobView from '@/views/EditJobView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,10 @@ const router = createRouter({
       path: '/jobs/:id',
       name: 'job',
       component:JobView,
+    },{
+      path: '/jobs/edit/:id',
+      name: 'editJob',
+      component:EditJobView,
     },{
       path: '/jobs/add',
       name: 'jobAdd',
---
+++ src/views/EditJobView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+EditJob
+</template>
+
+<style scoped>
+
+</style>
